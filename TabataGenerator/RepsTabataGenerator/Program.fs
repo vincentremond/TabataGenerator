af263e9e32a4354407b23cbf9e33d27b47fa5919
Vincent Rémond <vincent.remond@gmail.com>
2021-02-12 19:04:41 +0100

    WIP

--- TabataGenerator/RepsTabataGenerator/Program.fs
+++ TabataGenerator/RepsTabataGenerator/Program.fs
@@ -1,14 +1,9 @@
 ﻿namespace RepsTabataGenerator
 
-open System.Text.Json
-
 module EntryPoint =
     
     let writeToFile path contents =
         System.IO.File.WriteAllText(path, contents)
-        
-    let serialize obj =
-        JsonSerializer.Serialize (obj, JsonSerializerOptions( WriteIndented = true )) 
 
     [<EntryPoint>]
     let main argv =
@@ -17,7 +12,7 @@ module EntryPoint =
         |> WorkoutConfigurationConverter.convertConfig
         |> Array.map WorkoutIntervalExpander.create 
         |> OutputFileFormat.createResult
-        |> serialize
+        |> OutputSerialization.serialize
         |> writeToFile "result.workout"
 
         0 // return an integer exit code
