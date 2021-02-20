0cbbc00fe736410ae21969d2eeae6231272a36b5
Vincent Rémond <vincent.remond@gmail.com>
2021-02-20 11:52:42 +0100

    Improve BPM pace

--- TabataGenerator/RepsTabataGenerator/WorkoutIntervalExpander.fs
+++ TabataGenerator/RepsTabataGenerator/WorkoutIntervalExpander.fs
@@ -22,21 +22,21 @@ module WorkoutIntervalExpander =
             Intervals: DetailedInterval array
         }
 
-    let getRepsCount bpm (duration: Duration): Reps =
-        Convert.ToInt32(Math.Ceiling((float bpm) * duration.TotalSeconds / 60.))
+    let getRepsCount bpm (duration: Duration) bpmAdjust: Reps =
+        Convert.ToInt32(Math.Ceiling(bpmAdjust * (float bpm) * duration.TotalSeconds / 60.))
 
-    let createRepsInterval label (bpm: BPM) gif duration =
-        DetailedInterval.WorkReps(label, (getRepsCount bpm duration), bpm, gif)
+    let createRepsInterval label (bpm: BPM) gif duration bpmAdjust =
+        DetailedInterval.WorkReps(label, (getRepsCount bpm duration bpmAdjust), bpm, gif)
 
     let private createLabel pre exi exc cyi cyc lab =
         $"{pre}\n[Ex. {exi}/{exc} • Cycle {cyi}/{cyc}]\n{lab}"
 
-    let createInterval ex pre exi exc cyi cyc d: DetailedInterval =
+    let createInterval ex pre exi exc cyi cyc d bpmAdjust: DetailedInterval =
         let createLabel' lab = createLabel pre exi exc cyi cyc lab
 
         match ex with
         | ExerciseDuration (l) -> DetailedInterval.WorkDuration((createLabel' l), d)
-        | ExerciseReps (l, b, g) -> createRepsInterval (createLabel' l) b g d
+        | ExerciseReps (l, b, g) -> createRepsInterval (createLabel' l) b g d bpmAdjust
 
     let mapi (arr: Exercise array) =
         let mapi' index item = ((index + 1), item)
@@ -64,7 +64,7 @@ module WorkoutIntervalExpander =
                     let cyc = warmup
                     for cyi = 1 to cyc do
                         for (exi, exercise) in (description.Exercises |> mapi) do
-                            yield createInterval exercise "Warmup" exi exc cyi cyc description.Work
+                            yield createInterval exercise "Warmup" exi exc cyi cyc description.Work 0.75
                             yield postExerciseInterval true exi exc cyi cyc description
 
                 | _ -> ()
@@ -72,7 +72,7 @@ module WorkoutIntervalExpander =
                 let cyc = description.Cycles
                 for cyi = 1 to cyc do
                     for (exi, exercise) in (description.Exercises |> mapi) do
-                        yield createInterval exercise "" exi exc cyi cyc description.Work
+                        yield createInterval exercise "" exi exc cyi cyc description.Work 1.0
                         yield postExerciseInterval false exi exc cyi cyc description
             }
 
